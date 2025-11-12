0b854eefca6c876122ce2db8d3dd3bacc8a456ea
jaydenc390 <jaydenc390@nycstudents.net>
2025-11-12 13:20:08 -0500

    Excercise 1 & HTML Finished

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+const main = document.querySelector("main");
 
+const posts = [
+    { image: "", title: "The Basics of Semantic HTML", post: "" },
+    { image: "", title: "Advanced Semantic HTML", post: "" }
+];
